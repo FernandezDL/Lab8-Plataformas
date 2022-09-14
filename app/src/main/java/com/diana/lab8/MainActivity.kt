b1716c704ee20dae70c0b72630f5b62e7d2d0e3c
FernandezDL <fer21747@uvg.edu.gt>
2022-09-13 21:02:40 -0600

    nuevos cambios

--- app/src/main/java/com/diana/lab8/MainActivity.kt
+++ app/src/main/java/com/diana/lab8/MainActivity.kt
@@ -2,8 +2,13 @@ package com.diana.lab8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolBar: ToolBar
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/diana/lab8/ToolBar.kt
+++ app/src/main/java/com/diana/lab8/ToolBar.kt
@@ -5,13 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 
-class ToolBar : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tool_bar, container, false)
+class ToolBar : AppCompatActivity() {
+
+    private lateinit var toolBar: ToolBar
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_tool_bar)
+
+        val navigation = supportFragmentManager.findFragmentById(R.id.toolBarFragment_containerView)
+                as NavigationFragment
+        navController=navigation.navController
+
     }
 }
