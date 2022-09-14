b089c1176d8256a9e79745b4a3ef911ba848e70b
FernandezDL <fer21747@uvg.edu.gt>
2022-09-13 21:27:32 -0600

    cambios en el ToolBar y CharacterDetails

--- app/src/main/java/com/diana/lab8/ToolBar.kt
+++ app/src/main/java/com/diana/lab8/ToolBar.kt
@@ -1,16 +1,17 @@
 package com.diana.lab8
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
 
 class ToolBar : AppCompatActivity() {
 
-    private lateinit var toolBar: ToolBar
+    private lateinit var toolBar: MaterialToolbar
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +19,34 @@ class ToolBar : AppCompatActivity() {
         setContentView(R.layout.fragment_tool_bar)
 
         val navigation = supportFragmentManager.findFragmentById(R.id.toolBarFragment_containerView)
-                as NavigationFragment
+                as NavHostFragment
+
         navController=navigation.navController
 
+        val appBarConfiguration= AppBarConfiguration(navController.graph)
+
+        toolBar=findViewById(R.id.toolBar)
+        toolBar= setupWithNavController(navController, appBarConfiguration)
+
+        listenToNavGraphChanges()
+    }
+
+    private fun listenToNavGraphChanges() {
+
+        navController.addOnDestinationChangedListener{_, destino, _ ->
+            when(destino.id)
+            {
+                R.id.characterDetails2 ->
+                {
+                    toolBar.menu.findItem(R.id.menu_fromAToZ).isVisible= false
+                    toolBar.menu.findItem(R.id.menu_fromZToA).isVisible= false
+                }
+
+                else ->
+                {
+                    toolBar.visibility= View.VISIBLE
+                }
+            }
+        }
     }
 }
