352f76b570be87f48bc7c683215e6a5823a3d26e
FernandezDL <fer21747@uvg.edu.gt>
2022-09-13 23:50:16 -0600

    cambios con errores

--- app/src/main/java/com/diana/lab8/CharactersFragment.kt
+++ app/src/main/java/com/diana/lab8/CharactersFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class CharactersFragment : Fragment() {
+class CharactersFragment : Fragment(R.layout.fragment_characters) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
